7f3300c40337790ca2db7251e26da70221d33a93
AbhigyaWangoo <abhigyawangoo@gmail.com>
2024-02-21 18:22:59 -0800

    Cleaned up testes

--- test/test_basic.py
+++
@@ -1,9 +0,0 @@
-from classifier.decisiontree import ExplainableTreeClassifier
-
-def test_invalid_file_construction():
-    """ Tests whether an invalid file has been used to load the classifier """
-    try:
-        ExplainableTreeClassifier([], "", "nonexistant_file")
-        assert False
-    except NameError:
-        assert True
