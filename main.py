86136d32c0823962bf14d87e94d5bf23cb2d0168
AbhigyaWangoo <abhigyawangoo@gmail.com>
2024-03-06 08:51:41 -0800

    Added random binding fn

--- main.py
+++ main.py
@@ -4,13 +4,12 @@ from llm.client.runpod import RunPodClient
 from llm.client.mistral import MistralLLMClient
 from llm.client.claude import ClaudeClient
 
-if __name__ == "__main__":
-    # client = GPTClient() # GPT4
-    # client = MistralLLMClient() # MISTRAL
-    client=ClaudeClient()# Claude 3
-    # client = RunPodClient() # Llama 2
-    # print(cclient.query("Hello! How are you today?"))
-    generator = DatasetGenerator("rankings-claude.json", client, 188, "data/fewshotexamples.json")
+from query_engine.src.db import postgres_client
 
-    generator.generate_dataset()
-    print("Done generating dataset!")
+if __name__ == "__main__":
+    client = postgres_client.PostgresClient(135)
+    client.assign_random_bindings()
+    
+    # generator = DatasetGenerator("rankings-claude.json", client, 188, "data/fewshotexamples.json")
+    # generator.generate_dataset()
+    # print("Done generating dataset!")
