863484a652c50d26606fe14125772afd92e6ce81
AbhigyaWangoo <abhigyawangoo@gmail.com>
2024-03-06 17:48:58 -0800

    Multiprocessed dataset generation

--- main.py
+++ main.py
@@ -4,12 +4,36 @@ from llm.client.runpod import RunPodClient
 from llm.client.mistral import MistralLLMClient
 from llm.client.claude import ClaudeClient
 
-from query_engine.src.db import postgres_client
+import multiprocessing
+
+CLIENTS = {
+    "gpt4": GPTClient(),
+    "mistral": MistralLLMClient(),
+    "llama2": RunPodClient(),
+    "claude": ClaudeClient()
+}
+
+def gen_dataset(name: str):
+    """ Worker function to help generate dataset """
+    client_name = name.split("-")[-1]
+    client = CLIENTS[client_name]
+    print(client)
+
+    generator = DatasetGenerator(f"rankings-{name}.json", client, 135, "data/fewshotexamples.json")
+    generator.generate_dataset()
 
 if __name__ == "__main__":
-    client = postgres_client.PostgresClient(135)
-    client.assign_random_bindings()
+    multiprocessing.set_start_method('spawn')
 
-    # generator = DatasetGenerator("rankings-claude.json", client, 188, "data/fewshotexamples.json")
-    # generator.generate_dataset()
-    # print("Done generating dataset!")
+    procs=[]
+    # Spawn subprocesses
+    for key, _ in CLIENTS.items():
+        name = key
+
+        process = multiprocessing.Process(target=gen_dataset, args=(f"frontend-{name}", ))
+        procs.append(process)
+        process.start()
+
+    # Wait for all processes to complete
+    for process in procs:
+        process.join()
