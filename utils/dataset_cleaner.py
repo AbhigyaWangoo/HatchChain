753bbb596d3c005d149f6b6cb76b8f9359494af1
AbhigyaWangoo <abhigyawangoo@gmail.com>
2024-03-16 14:57:16 +0530

    Added additional dataset cleaner features

--- utils/dataset_cleaner.py
+++ utils/dataset_cleaner.py
@@ -1,6 +1,8 @@
 import os
 import json
 import csv
+from query_engine.src.db import postgres_client
+import tqdm
 
 DIRSRC="reports"
 
@@ -31,10 +33,48 @@ def process_directory(directory_path):
                 except Exception as e:
                     print(f"error writing customer back: {e}")
 
-def convert_to_csv(json_file: str):
-    """Converts the provided json file into a csv"""
-    headers=["name", "linkedin links", "reasoning"]
-    with open(csv_file, 'w', newline='') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=headers)
+def convert_to_csv(directory_path: str = DIRSRC):
+    """Converts all the provided json files into csv files"""
+    name_to_jobid={
+        "ml":148,
+        "frontend":135,
+        "backend":188,
+        "pm":172
+    }
 
-process_directory(DIRSRC)
+    headers=["name", "linkedin links", "reasoning"]
+    for root, dirs, files in tqdm.tqdm(os.walk(directory_path), desc="Walking through directories"):
+        for file_name in tqdm.tqdm(files, desc="Walking through json files"):
+            if file_name.endswith('.json'):
+                file_path = os.path.join(root, file_name)
+                basename = file_name.split(".")[0]
+                csv_file = os.path.join(root, f"{basename}.csv")
+                
+                type=basename.split("-")[1]
+                job_id=name_to_jobid[type]
+                client=postgres_client.PostgresClient(job_id)
+
+                try:
+                    with open(file_path, "r", encoding="utf8") as fp:
+                        dataset = json.load(fp)
+                        dataset_data = dataset["dataset"]
+
+                        with open(csv_file, 'w', newline='', encoding="utf8") as csvfile:
+                            dict_writer = csv.DictWriter(csvfile, fieldnames=headers, delimiter=",")
+                            dict_writer.writeheader()
+
+                            for resume_explanation in tqdm.tqdm(dataset_data, desc=f"Generating dataset for file {basename}"):
+                                resume=resume_explanation["resume"]
+                                explanation=resume_explanation["explanation"]
+
+                                name=resume[1].get("name", None)
+                                links=resume[1].get("links", None)
+                                if links is None:
+                                    links = client.read_candidate(resume[0], "resume_data")[0]["links"]
+                                links = '______'.join(links)
+
+                                csv_row={f"{headers[0]}": name, f"{headers[1]}": links, f"{headers[2]}": explanation}
+                                dict_writer.writerow(csv_row)
+
+                except Exception as e:
+                    print(f"error converting to csv: {e}")
